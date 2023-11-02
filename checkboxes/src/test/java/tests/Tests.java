94b4586488b635a056750f5184223f7540700e8b
Stephanie Peñaloza <spenalozacortes@gmail.com>
2023-11-02 16:09:57 -0600

    Test select checkbox

--- checkboxes/src/test/java/tests/Tests.java
+++ checkboxes/src/test/java/tests/Tests.java
@@ -1,4 +1,35 @@
 package tests;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
 public class Tests {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setup() {
+        driver = new ChromeDriver();
+        driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+        driver.manage().window().maximize();
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
+
+    @Test
+    public void test() {
+        WebElement checkbox = driver.findElement(By.id("checkBoxOption1"));
+        checkbox.click();
+        Assert.assertTrue(checkbox.isSelected(), "Checkbox is not selected!");
+        checkbox.click();
+        Assert.assertFalse(checkbox.isSelected(), "Checkbox is selected!");
+    }
 }
